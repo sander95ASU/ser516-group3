42a70ca40cd2a787de46e3b582b07e900b2b42ec
andrewbaruck <andrewbaruck@icloud.com>
2026-02-19 19:44:25 -0700

    commiting and pushing so I can move to working on my other machine

--- AfferentEfferentService/src/main/java/org/service/Main.java
+++ AfferentEfferentService/src/main/java/org/service/Main.java
@@ -5,22 +5,24 @@ import org.github.githubLoginObject;
 
 public class Main {
 
-    Scanner scanner = new Scanner(System.in);
+
     githubLoginObject GLO = new githubLoginObject();
     public int choice = 1;
-    void main(String[] args) {
+
+    public static void main(String[] args) {
         welcomeUser();
 
     }
 
 
 
-public void welcomeUser(){
-    System.out.println("Welcome to the Afferent Efferent Service!");
+public static void welcomeUser(){
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Afferent Efferent Service!");
     System.out.println("Please select an option:");
     System.out.println("1. Check from local files");
     System.out.println("2. Check from github");
-    choice = scanner.nextInt();
+    int choice = scanner.nextInt();
 
 }
 
